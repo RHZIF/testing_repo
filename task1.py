068b3164c088dbc42bbc0e044e623cbaff363378
Yassine Rhzif <Rhzif@hotmail.com>
2021-11-30 21:08:56 +0100

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pandas as pd 
+import json 
+import requests
+import codecs
+import yfinance as yf
+import base64
+
+
+
+st.title("Portfolio simulation dashboard")
+tickers = {"atvi","adbe","amd","alxn","algn","goog","googl","amzn","amgn","adi","anss","aapl","amat","amat","asml","adsk","adp","bidu","biib","bmrn","bkng","avgo","cdns","cdw","cern","chtr","chkp","ctas","csco","ctxs","ctsh","cmcsa","cprt","cost","csx","dxcm","docu","dltr","ebay","ea","exc","expe","fb","fast","fisv","fox","foxa","gild","idxx","ilmn","incy","intc","intu","isrg","jd","klac","lrcx","lbtya","lbtyk","lulu","mar","mxim","meli","mchp","mu","msft","mrna","mdlz","mnst","ntes","nflx","nvda","nxpi","orly","pcar","payx","pypl","pep","pdd","qcom","regn","rost","sgen","siri","swks","splk","sbux","snps","tmus","ttwo","tsla","txn","khc","tcom","ulta","vrsn","vrsk","vrtx","wba","wdc","wday","xel","xlnx","zm"}
+dropdown = st.selectbox("Pick your assets", tickers)
+start = st.date_input('Start', value = pd.to_datetime('2000-01-01'))
+end = st.date_input('End', value = pd.to_datetime('today'))
+
+
+df = yf.download(dropdown,
+                      start, 
+                      end, 
+                      progress=False)
+
+
+st.line_chart(df.Close)
+
+st.dataframe(df)
+
+
+def convert_df(tdf):
+    return df.to_csv().encode('utf-8')
+
+csv = convert_df(df)
+
+st.download_button(
+     label="Download data as CSV",
+     data=csv,
+     file_name= str(dropdown) +'.csv',
+     mime='text/csv',
+ )
